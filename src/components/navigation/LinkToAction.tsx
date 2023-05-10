76edfb3a9122ddc5f54a1c2d69d893578bfa7d75
saufth <saufth@gmail.com>
2023-05-09 22:11:12 -0600

    Framework version 1.0.4

--- src/components/navigation/LinkToAction.tsx
+++ src/components/navigation/LinkToAction.tsx
@@ -7,6 +7,8 @@ import type { Themeable } from '@/types/theme'
 
 /**
  * The main link to action component of the application
+ * @see {@link Themeable} for props specifications
+ * @param Themeable The component props
  * @returns The LinkToAction component
  */
 export default function LinkToAction ({ theme }: Themeable) {
